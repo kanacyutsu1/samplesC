423d6ee3ba61737af83dbb81c3d842781bcd3f31
care <kanacyutsu@gmail.com>
2025-09-16 23:40:12 +0700

    Update form_widgets.dart

--- form_app/lib/src/form_widgets.dart
+++ form_app/lib/src/form_widgets.dart
@@ -13,7 +13,6 @@ class FormWidgetsDemo extends StatefulWidget {
 }
 
 class _FormWidgetsDemoState extends State<FormWidgetsDemo> {
-  final _formKey = GlobalKey<FormState>();
   String title = '';
   String description = '';
   DateTime date = DateTime.now();
@@ -25,191 +24,46 @@ class _FormWidgetsDemoState extends State<FormWidgetsDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Form widgets')),
-      body: Form(
-        key: _formKey,
-        child: Scrollbar(
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Card(
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(16),
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxWidth: 400),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ...[
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            filled: true,
-                            hintText: 'Enter a title...',
-                            labelText: 'Title',
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              title = value;
-                            });
-                          },
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            filled: true,
-                            hintText: 'Enter a description...',
-                            labelText: 'Description',
-                          ),
-                          onChanged: (value) {
-                            description = value;
-                          },
-                          maxLines: 5,
-                        ),
-                        _FormDatePicker(
-                          date: date,
-                          onChanged: (value) {
-                            setState(() {
-                              date = value;
-                            });
-                          },
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment:
-                                  MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Estimated value',
-                                  style: Theme.of(
-                                    context,
-                                  ).textTheme.bodyLarge,
-                                ),
-                              ],
-                            ),
-                            Text(
-                              intl.NumberFormat.currency(
-                                symbol: "\$",
-                                decimalDigits: 0,
-                              ).format(maxValue),
-                              style: Theme.of(
-                                context,
-                              ).textTheme.titleMedium,
-                            ),
-                            Slider(
-                              min: 0,
-                              max: 500,
-                              divisions: 500,
-                              value: maxValue,
-                              onChanged: (value) {
-                                setState(() {
-                                  maxValue = value;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Checkbox(
-                              value: brushedTeeth,
-                              onChanged: (checked) {
-                                setState(() {
-                                  brushedTeeth = checked;
-                                });
-                              },
-                            ),
-                            Text(
-                              'Brushed Teeth',
-                              style: Theme.of(
-                                context,
-                              ).textTheme.titleMedium,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Enable feature',
-                              style: Theme.of(context).textTheme.bodyLarge,
-                            ),
-                            Switch(
-                              value: enableFeature,
-                              onChanged: (enabled) {
-                                setState(() {
-                                  enableFeature = enabled;
-                                });
-                              },
-                            ),
-                          ],
-                        ),
-                      ].expand(
-                        (widget) => [widget, const SizedBox(height: 24)],
-                      ),
-                    ],
-                  ),
+      body: Card(
+        child: 
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Column(
+              children: [
+                
+                    Text(
+                      intl.NumberFormat.currency(
+                        symbol: "\$",
+                        decimalDigits: 0,
+                      ).format(maxValue)
+                    ),
+                    Slider(
+                      min: 0,
+                      max: 10,
+                      divisions: 100,
+                      value: maxValue,
+                      onChanged: (value) {
+                        setState(() {
+                          maxValue = value;
+                        });
+                      },
+                    ),
+                  
+                
+                Switch(
+                  value: enableFeature,
+                  onChanged: (enabled) {
+                    setState(() {
+                      enableFeature = enabled;
+                    });
+                  },
                 ),
-              ),
+              ],
             ),
           ),
-        ),
       ),
     );
   }
 }
 
-class _FormDatePicker extends StatefulWidget {
-  final DateTime date;
-  final ValueChanged<DateTime> onChanged;
 
-  const _FormDatePicker({required this.date, required this.onChanged});
-
-  @override
-  State<_FormDatePicker> createState() => _FormDatePickerState();
-}
-
-class _FormDatePickerState extends State<_FormDatePicker> {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text('Date', style: Theme.of(context).textTheme.bodyLarge),
-            Text(
-              intl.DateFormat.yMd().format(widget.date),
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ],
-        ),
-        TextButton(
-          child: const Text('Edit'),
-          onPressed: () async {
-            var newDate = await showDatePicker(
-              context: context,
-              initialDate: widget.date,
-              firstDate: DateTime(1900),
-              lastDate: DateTime(2100),
-            );
-
-            // Don't change the date if the date picker returns null.
-            if (newDate == null) {
-              return;
-            }
-
-            widget.onChanged(newDate);
-          },
-        ),
-      ],
-    );
-  }
-}
